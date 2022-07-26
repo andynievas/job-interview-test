0c4bae879c7d16a6322912a1bbeb0bcfbb3e47c3
Andy Nievas <68872131+andynievas@users.noreply.github.com>
2022-07-26 03:13:26 -0300

    Update home.jsx

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -5,17 +5,20 @@ import urlBase from "../urlBase";
 import utils from "../utils";
 
 function Characters() {
-
+  
+  const [isLoading, setIsLoading] = useState(false);
   const [characters, setCharacters] = useState(null);
   const [page, setPage] = useState(1);
   const urlApi = `${urlBase}/people?page=${page}`
 
   useEffect(() => {
     const getData = async () => {
+      setIsLoading(true);
       const response = await fetch(urlApi);
       if (response.status !== 200) return;
       const data = await response.json();
       setCharacters(data.results);
+      setIsLoading(false);
     }
     getData();
 
@@ -33,7 +36,7 @@ function Characters() {
 
   return <div className="container" >
     <h2>Characters list</h2>
-    <CharactersList list={characters} favorites={favs} />
+    <CharactersList isLoading={isLoading} list={characters} favorites={favs} />
     <div className="flex" >
       <div className="flex flex-align-center margin-y-sm margin-auto" >
         <button onClick={() => handleChangePage(-1)}
